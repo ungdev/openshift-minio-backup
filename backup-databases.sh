66d7ce1d9e14a77325cec5ecdc832ade633ac469
Christian d'Autume <christian@dautume.fr>
2019-02-04 17:32:10 +0100

    Change directory structure

--- backup-databases.sh
+++ backup-databases.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DATE=$(date +%Y-%m-%d_%H-%M-%S)
+DATE=$(date +%Y-%m-%d_%H-%M)
 DIR="/tmp/backup"
 
 #BACKUP_HOST=""
@@ -9,7 +9,8 @@ DIR="/tmp/backup"
 putBackup ()
 {
   file=$1
-  resource="/${BUCKET}/${file}"
+  chemin=$2
+  resource="/${BUCKET}/${chemin}"
   content_type="application/octet-stream"
   date=$(date -R)
 
@@ -57,19 +58,19 @@ do
         DBNAME=$(oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'echo $MYSQL_DATABASE')
         echo "Backup database $DBNAME..."
         oc -n $PROJECT exec $POD -- /bin/bash -c 'mysqldump -h 127.0.0.1 -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE' | gzip > $DIR/$PROJECT-$DBNAME-$DATE.sql.gz
-        putBackup $DIR/$PROJECT-$DBNAME-$DATE.sql.gz
+        putBackup $DIR/$PROJECT-$DBNAME-$DATE.sql.gz mysql/$PROJECT/$DBNAME-$DATE.sql.gz
         ;;
       postgresql)
         DBNAME=$(oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'echo $POSTGRESQL_DATABASE')
         echo "Backup database $DBNAME..."
         oc -n $PROJECT exec $POD -- /bin/bash -c 'pg_dump -Fc $POSTGRESQL_DATABASE ' | gzip > $DIR/$PROJECT-$DBNAME-$DATE.pg.gz
-        putBackup $DIR/$PROJECT-$DBNAME-$DATE.pg.gz
+        putBackup $DIR/$PROJECT-$DBNAME-$DATE.pg.gz pgsql/$PROJECT/$DBNAME-$DATE.pg.gz
         ;;
       mongodb)
         DBNAME=$(oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'echo $MONGODB_DATABASE')
         echo "Backup database $DBNAME..."
         oc -n $PROJECT exec $POD -- /bin/bash -c 'mongodump -u $MONGODB_USER -p $MONGODB_PASSWORD -d $MONGODB_DATABASE --gzip --archive' > $DIR/$PROJECT-$DBNAME-$DATE.mongodump.gz
-        putBackup $DIR/$PROJECT-$DBNAME-$DATE.mongodump.gz
+        putBackup $DIR/$PROJECT-$DBNAME-$DATE.mongodump.gz mongodb/$PROJECT/$DBNAME-$DATE.mongodump.gz
         ;;
       *)
         echo "ERROR: Unknown backup-method $i"
