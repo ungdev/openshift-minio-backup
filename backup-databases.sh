eeef64de6d9c89d0fb62c346ebdd991dcc3d88b4
Christian d'Autume <christian@dautume.fr>
2019-02-04 17:22:25 +0100

    Let's enable put Backup

--- backup-databases.sh
+++ backup-databases.sh
@@ -57,19 +57,19 @@ do
         DBNAME=$(oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'echo $MYSQL_DATABASE')
         echo "Backup database $DBNAME..."
         oc -n $PROJECT exec $POD -- /bin/bash -c 'mysqldump -h 127.0.0.1 -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_DATABASE' | gzip > $DIR/$PROJECT-$DBNAME-$DATE.sql.gz
-        #putBackup $DIR/$PROJECT-$DBNAME-$DATE.sql.gz
+        putBackup $DIR/$PROJECT-$DBNAME-$DATE.sql.gz
         ;;
       postgresql)
         DBNAME=$(oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'echo $POSTGRESQL_DATABASE')
         echo "Backup database $DBNAME..."
         oc -n $PROJECT exec $POD -- /bin/bash -c 'pg_dump -Fc $POSTGRESQL_DATABASE ' | gzip > $DIR/$PROJECT-$DBNAME-$DATE.pg.gz
-        #putBackup $DIR/$PROJECT-$DBNAME-$DATE.pg.gz
+        putBackup $DIR/$PROJECT-$DBNAME-$DATE.pg.gz
         ;;
       mongodb)
         DBNAME=$(oc -n $PROJECT exec $POD -- /usr/bin/sh -c 'echo $MONGODB_DATABASE')
         echo "Backup database $DBNAME..."
         oc -n $PROJECT exec $POD -- /bin/bash -c 'mongodump -u $MONGODB_USER -p $MONGODB_PASSWORD -d $MONGODB_DATABASE --gzip --archive' > $DIR/$PROJECT-$DBNAME-$DATE.mongodump.gz
-        #putBackup $DIR/$PROJECT-$DBNAME-$DATE.mongodump.gz
+        putBackup $DIR/$PROJECT-$DBNAME-$DATE.mongodump.gz
         ;;
       *)
         echo "ERROR: Unknown backup-method $i"
